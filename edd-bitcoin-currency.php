b4fa7490f373e3b3880ae72ad9e5a7d98f66d554
claudiosmweb <contato@claudiosmweb.com>
2014-01-09 23:57:08 -0200

    fixed standards

--- edd-bitcoin-currency.php
+++ edd-bitcoin-currency.php
@@ -16,69 +16,67 @@
  */
 class EDD_BTC_Currency {
 
-    /**
-     * Class construct.
-     */
-    public function __construct() {
+	/**
+	 * Class construct.
+	 */
+	public function __construct() {
 
-        // Actions.
-        add_action( 'plugins_loaded', array( $this, 'load_textdomain' ), 0 );
+		// Actions.
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ), 0 );
 
-        // Filters.
-        add_filter( 'edd_currencies', array( $this, 'add_currency' ) );
-        add_filter( 'edd_btc_currency_filter_before', array( $this, 'currency_symbol' ) );
-        add_filter( 'edd_btc_currency_filter_after', array( $this, 'currency_symbol' ) );
-        add_filter( 'edd_format_amount_decimals', array( $this, 'currency_decimals' ) );
-    }
+		// Filters.
+		add_filter( 'edd_currencies', array( $this, 'add_currency' ) );
+		add_filter( 'edd_btc_currency_filter_before', array( $this, 'currency_symbol' ) );
+		add_filter( 'edd_btc_currency_filter_after', array( $this, 'currency_symbol' ) );
+		add_filter( 'edd_format_amount_decimals', array( $this, 'currency_decimals' ) );
+	}
 
-    /**
-     * Load Plugin textdomain.
-     *
-     * @return void
-     */
-    public function load_textdomain() {
-        load_plugin_textdomain( 'eddbitcc', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
-    }
+	/**
+	 * Load Plugin textdomain.
+	 *
+	 * @return void
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'eddbitcc', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
 
-    /**
-     * Add Bitcoin currency.
-     *
-     * @param  array $currencies Default currencies.
-     *
-     * @return array             News currencies with Bitcoin.
-     */
-    public function add_currency( $currencies ) {
-        $currencies['BTC'] = __( 'Bitcoin (&#3647;)', 'eddbitcc' );
-        asort( $currencies );
+	/**
+	 * Add Bitcoin currency.
+	 *
+	 * @param  array $currencies Default currencies.
+	 *
+	 * @return array             News currencies with Bitcoin.
+	 */
+	public function add_currency( $currencies ) {
+		$currencies['BTC'] = __( 'Bitcoin (&#3647;)', 'eddbitcc' );
+		asort( $currencies );
 
-        return $currencies;
-    }
+		return $currencies;
+	}
 
-    /**
-     * Fix Bitcoin currency symbol.
-     *
-     * @param  string $formated Formated currency display.
-     *
-     * @return string           Fixed Bitcoin currency display.
-     */
-    public function currency_symbol( $formated ) {
-        $currency_symbol = apply_filters( 'bitcoin_currency_symbol', '&#3647;' );
+	/**
+	 * Fix Bitcoin currency symbol.
+	 *
+	 * @param  string $formated Formated currency display.
+	 *
+	 * @return string           Fixed Bitcoin currency display.
+	 */
+	public function currency_symbol( $formated ) {
+		$currency_symbol = apply_filters( 'bitcoin_currency_symbol', '&#3647;' );
 
-        $formated = str_replace( 'BTC', $currency_symbol, $formated );
+		$formated = str_replace( 'BTC', $currency_symbol, $formated );
 
-        return $formated;
-    }
+		return $formated;
+	}
 
-    /**
-     * Fix decimals to Bitcoin.
-     *
-     * @return int           Bitcoin decimals.
-     */
-    public function currency_decimals() {
-
-        return 8;
-    }
-
+	/**
+	 * Fix decimals to Bitcoin.
+	 *
+	 * @return int           Bitcoin decimals.
+	 */
+	public function currency_decimals() {
+		return 8;
+	}
 }
 
-$edd_btc_currency = new EDD_BTC_Currency();
+new EDD_BTC_Currency();
